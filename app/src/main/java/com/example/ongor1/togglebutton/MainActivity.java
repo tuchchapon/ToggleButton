28b50bb6b270d021f55129e905c59749e3605fa6
AnNoi <annoi@Macbook-Pro-khxng-AnNoi.local>
2018-03-30 07:21:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ongor1/togglebutton/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ongor1.togglebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private TextView resultView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultView = (TextView)findViewById(R.id.resultView);
+
+        toggleButton = (ToggleButton)findViewById(R.id.toggleButtonID);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    // toggle is enabled
+                    resultView.setVisibility(View.VISIBLE);
+                } else {
+                    // toggle is disabled
+                    resultView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
